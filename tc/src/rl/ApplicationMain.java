b21c6b1af74a6d26f71680ea4bf162036be26098
CByrd17 <cbyrd17@yahoo.com>
2015-11-18 18:00:27 -0500

    Lesson 1 of Trystans roguelike-tutorial. http://trystans.blogspot.com/2011/08/roguelike-tutorial-01-java-eclipse.html

---
+++ tc/src/rl/ApplicationMain.java
@@ -0,0 +1,29 @@
+package rl;
+
+import javax.swing.JFrame;
+import asciiPanel.AsciiPanel;
+
+public class ApplicationMain extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private AsciiPanel terminal;
+
+	public ApplicationMain() {
+		super();
+		terminal = new AsciiPanel();
+		terminal.write("rl tutorial", 1, 1);
+		add(terminal);
+		pack();
+	}
+
+	public static void main(String[] args) {
+		ApplicationMain app = new ApplicationMain();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setVisible(true);
+	}
+
+}
